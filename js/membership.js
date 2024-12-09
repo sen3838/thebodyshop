0122c30a8332b4990dd87167124a242f4cf7bd3f
sen3838 <tpsk9972@naver.com>
2024-12-09 17:40:38 +0900

    1740

--- js/membership.js
+++ js/membership.js
@@ -1,4 +1,17 @@
 $(function () {
+  // 헤더 메뉴
+  $("header .bar").on("click", function () {
+    $("header .menu_wrap,header .menu_wrap > ul")
+      .stop()
+      .animate({ right: "0" });
+  });
+
+  $("header .close").on("click", function () {
+    $("header .menu_wrap,header .menu_wrap > ul")
+      .stop()
+      .animate({ right: "-100%" });
+  });
+
   $(".title").addClass("on");
   let baseline = -300;
   let title = $(".title").offset().top;
@@ -9,6 +22,10 @@ $(function () {
   $(window).on("scroll", function () {
     let scroll = $(this).scrollTop();
 
+    $("header").addClass("on");
+    if (scroll <= title) {
+      $("header").removeClass("on");
+    }
     if (scroll >= con1) {
       $(".con1").addClass("on");
     }
